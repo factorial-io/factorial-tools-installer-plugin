69e4ffe5295c9e0c768cbb7724413394f7a3010e
Stephan Maximilian Huber <stephan@factorial.io>
2015-09-27 17:47:49 +0200

    Remove vendor from path.

--- src/FactorialTools/Composer/ToolsInstaller.php
+++ src/FactorialTools/Composer/ToolsInstaller.php
@@ -7,19 +7,28 @@ use Composer\Installer\LibraryInstaller;
 
 class ToolsInstaller extends LibraryInstaller
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function getPackageBasePath(PackageInterface $package)
-    {
-        return '_tools/'.$package->getPrettyName();
+  /**
+   * {@inheritDoc}
+   */
+  public function getPackageBasePath(PackageInterface $package)
+  {
+    $needed_prefix = 'factorial-io/';
+    $prefix = substr($package->getPrettyName(), 0, strlen($needed_prefix));
+    if (needed_prefix !== $prefix) {
+      throw new \InvalidArgumentException(
+        'Unable to install factorial-tool. It'
+        .'should always start its package name with '
+        .'"'. $needed_prefix. '"'
+      );
     }
+    return '_tools/'.substr($package->getPrettyName(), strlen($needed_prefix));
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    public function supports($packageType)
-    {
-        return 'factorial-tools' === $packageType;
-    }
+  /**
+   * {@inheritDoc}
+   */
+  public function supports($packageType)
+  {
+    return 'factorial-tools' === $packageType;
+  }
 }
